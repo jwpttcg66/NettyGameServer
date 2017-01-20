97d06aac2ef195de18441cf898f22b9cd01605fe
jwp <jwpttcg66@126.com>
2017-01-20 16:18:39 +0800

    增加udp test

---
+++ src/test/java/com/wolf/shoot/udp/client/EchoClient.java
@@ -0,0 +1,10 @@
+package com.wolf.shoot.udp.client;
+
+/**
+ * Created by jwp on 2017/1/20.
+ */
+public class EchoClient {
+    public static void main(String[] args) throws Exception{
+        
+    }
+}
