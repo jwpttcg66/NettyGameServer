77d3e2f6206fd10ac51fe4fb17e1d7a489b8ea0f
jwp <jwpttcg66@126.com>
2017-01-22 15:19:47 +0800

    udp可以读了

--- src/test/java/com/wolf/shoot/udp/server/EchoServer.java
+++ src/test/java/com/wolf/shoot/udp/server/EchoServer.java
@@ -1,22 +1,50 @@
 package com.wolf.shoot.udp.server;
 
 
+import com.wolf.shoot.udp.client.UdpHandler;
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.DatagramPacket;
 import io.netty.channel.socket.nio.NioDatagramChannel;
+import io.netty.handler.codec.MessageToMessageDecoder;
+
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+import java.util.List;
 
 /**
  * Created by jwp on 2017/1/20.
  */
 public class EchoServer {
-    public static void main(String[] args)  throws Exception{
-        Bootstrap serverBootStrap = new Bootstrap();
+    public static void main(String[] args) throws Exception {
+        Bootstrap b = new Bootstrap();
         EventLoopGroup group = new NioEventLoopGroup();
-        serverBootStrap.group(group).channel(NioDatagramChannel.class).handler(new EchoServerHandler());
-        //绑定端口
-        int port = 9999;
-        serverBootStrap.bind("127.0.0.1", port).sync().channel().closeFuture().await();
+        b.group(group)
+                .channel(NioDatagramChannel.class)
+//                .handler(new ChannelInitializer<NioDatagramChannel>() {
+//
+//                    @Override
+//                    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+//                        super.channelActive(ctx);
+//                    }
+//
+//                    @Override
+//                    protected void initChannel(NioDatagramChannel ch) throws Exception {
+//                        ChannelPipeline cp = ch.pipeline();
+//                        cp.addLast("framer", new MessageToMessageDecoder<DatagramPacket>() {
+//                            @Override
+//                            protected void decode(ChannelHandlerContext ctx, DatagramPacket msg, List<Object> out) throws Exception {
+//                                String string = msg.content().toString(Charset.forName("UTF-8"));
+//                                out.add(msg.content().toString(Charset.forName("UTF-8")));
+//                                System.out.println(string);
+//                            }
+//                        }).addLast("handler", new EchoServerHandler());
+//                    }
+//                });
+                    .handler( new EchoServerHandler());
 
+        // 服务端监听在9999端口
+        b.bind(9999).sync().channel().closeFuture().await();
     }
 }
--- src/test/java/com/wolf/shoot/udp/server/EchoServerHandler.java
+++ src/test/java/com/wolf/shoot/udp/server/EchoServerHandler.java
@@ -3,12 +3,14 @@ package com.wolf.shoot.udp.server;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.socket.DatagramPacket;
 import io.netty.util.CharsetUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.DatagramPacket;
+import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 
 /**
  * Created by jwp on 2017/1/20.
@@ -19,15 +21,16 @@ public class EchoServerHandler extends SimpleChannelInboundHandler<DatagramPacke
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, DatagramPacket datagramPacket) throws Exception {
         //读取数据
-        ByteBuffer buf =  ByteBuffer.wrap(datagramPacket.getData());
-        ByteBuffer readBuffer = buf.asReadOnlyBuffer();
-        String body = new String(readBuffer.array(), CharsetUtil.UTF_8);
-        utilLogger.debug("收到客户端数据" + body);
+//        ByteBuffer buf =  ByteBuffer.wrap(datagramPacket.copy().content().array());
+//        ByteBuffer readBuffer = buf.asReadOnlyBuffer();
+//        String body = new String(readBuffer.array(), CharsetUtil.UTF_8);
+        String string = datagramPacket.content().toString(Charset.forName("UTF-8"));
+        utilLogger.debug("收到客户端数据" + string);
 
         //回复客户端
         String response = "Hello, 服务器事件为" + System.currentTimeMillis();
         byte[] bytes = response.getBytes(CharsetUtil.UTF_8);
-        DatagramPacket responsePacket = new DatagramPacket(bytes, bytes.length);
+        DatagramPacket responsePacket = new DatagramPacket(Unpooled.copiedBuffer(bytes), datagramPacket.sender());
         channelHandlerContext.writeAndFlush(response).sync();
     }
 }
