963e1244a0e7f9f04f31b14027bcceaf3ce30713
jwp <jwpttcg66@126.com>
2017-01-26 12:31:56 +0800

    增加proto编码工厂

---
+++ src/main/java/com/wolf/shoot/net/message/decoder/INetMessageEncoderFactory.java
@@ -0,0 +1,11 @@
+package com.wolf.shoot.net.message.decoder;
+
+import com.wolf.shoot.net.message.NetMessage;
+import io.netty.buffer.ByteBuf;
+
+/**
+ * Created by jwp on 2017/1/26.
+ */
+public interface INetMessageEncoderFactory {
+    public NetMessage praseMessage(ByteBuf byteBuf);
+}
