7666287df28cfe8fbce57313b113ef76c263300d
jiangwenping <jwpttcg66>
2017-02-06 11:44:56 +0800

    增加gameserver

---
+++ src/main/java/com/wolf/shoot/server/GameServer.java
@@ -0,0 +1,179 @@
+package com.wolf.shoot.server;
+
+import com.wolf.shoot.constant.GlobalConstants;
+import com.wolf.shoot.constant.Loggers;
+import com.wolf.shoot.service.AbstractServerService;
+import com.wolf.shoot.service.ServerServiceManager;
+import com.wolf.shoot.util.MemUtils;
+import org.slf4j.Logger;
+
+/**
+ * Created by jiangwenping on 17/2/6.
+ *
+ /**
+ *
+ *                 _oo0oo_
+                  o8888888o
+                  88" . "88
+                  (| -_- |)
+                  0\  =  /0
+                ___/`---'\___
+              .' \\|     |-- '.
+             / \\|||  :  |||-- \
+            / _||||| -:- |||||- \
+           |   | \\\  -  --/ |   |
+           | \_|  ''\---/''  |_/ |
+           \  .-\__  '-'  ___/-. /
+         ___'. .'  /--.--\  `. .'___
+      ."" '<  `.___\_<|>_/___.' >' "".
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+     \  \ `_.   \_ __\ /__ _/   .-` /  /
+ =====`-.____`.___ \_____/___.-`___.-'=====
+                   `=---='
+
+
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+              佛祖保佑    永无BUG
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ *
+ * 负责游戏服务器的初始化，基础资源的加载，服务器进程的启动
+ *
+ */
+public class GameServer extends AbstractServerService{
+
+
+    /** 日志 */
+    private static final Logger logger = Loggers.gameLogger;
+
+    /**
+     * @param cfgPath
+     *            主配置文件路径
+     */
+    public GameServer() {
+        super(ServerServiceManager.SERVICE_ID_ROOT);
+    }
+
+    /**
+     * 初始化各种资源和服务
+     *
+     * @throws Exception
+     */
+    public void init(String configFile) throws Exception {
+        logger.info("Begin to initialize Globals");
+//        Globals.init(configFile);
+//        ServerServiceManager.getInstance().onReady();
+//        CommunicationServerService.getInstance().onReady();
+        logger.info("Globals initialized");
+        this.initServer();
+    }
+
+
+    private void initServer() {
+
+    }
+
+    /**
+     * 启动服务器
+     *
+     * @throws IOException
+     */
+    public void start() throws Exception {
+
+        logger.info("Begin to start Globals");
+        logger.info("Globals started");
+
+        // 注册停服监听器，用于执行资源的销毁等停服时的处理工作
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                logger.info("Begin to shutdown Game Server ");
+                // 设置GameServer关闭状态
+                GameServerRuntime.setShutdowning();
+                ServerStatusLog.getDefaultLog().logRunning();
+                // 关闭游戏连接服务
+                try {
+//                    ServerService.getInstance().onDown();
+//                    logger.info("gameCommService.shutdown:ok");
+//
+//                    GameServerConfigServiceEx gameServerConfigService = (GameServerConfigServiceEx)LocalMananger.getInstance().getGameServerConfigService();
+//                    GameServerConfig cfg = gameServerConfigService.getGameServerConfig();
+//                    if(gameServerConfigService.getA5GameDynamicPropertiesConfig().getProperty(DynamicPropertiesEnum.COMMUNICATION_START_UP_OPEN.toString().toLowerCase(), true)) {
+//                        CommunicationServerService.getInstance().onDown();
+//                    }
+//                    logger.info("CommunicationServerService.shutdown:ok");
+//
+//                    Globals.stop();
+                    logger.info("Globals.shutdown:ok");
+                } catch (Exception e) {
+                    logger.error("close connector service exception:", e);
+                } catch (Error e) {
+                    logger.error("close connector service error:", e);
+                }
+
+                ServerStatusLog.getDefaultLog().logStopped();
+                // 注销性能收集
+                logger.info("Game Server shutdowned");
+            }
+        });
+        GameServerRuntime.setOpenOn();
+    }
+
+    public static void main(String[] args) {
+        logger.info("Starting Game Server");
+        logger.info(MemUtils.memoryInfo());
+        String configFile = GlobalConstants.ConfigFile.GAME_SERVER_CONIFG;
+        String admCmd = "";
+        String[] admArgs = null;
+
+        if(args != null && args.length == 1){
+            configFile = args[0];
+        }
+
+        if (args != null && args.length >= 2){
+            admCmd = args[1];
+            if (args.length >= 3) {
+                admArgs = new String[args.length - 2];
+                for (int i=0; i<admArgs.length; ++i) {
+                    admArgs[i] = args[i + 2];
+                }
+            }
+        }
+
+        if (admCmd.isEmpty()) {
+            try {
+                /**
+                 * 程序初始化程序缓存模块
+                 */
+
+                ServerStatusLog.getDefaultLog().logStarting();
+//                IoBuffer.setUseDirectBuffer(false);
+//                IoBuffer.setAllocator(new SimpleBufferAllocator());
+                GameServer server = new GameServer();
+
+                server.init(configFile);
+                server.start();
+                ServerStatusLog.getDefaultLog().logRunning();
+            } catch (Exception e) {
+                logger.error("Failed to start server", e);
+                System.err.println(e);
+                ServerStatusLog.getDefaultLog().logStartFail();
+                System.exit(1);
+                return;
+            }
+            logger.info(MemUtils.memoryInfo());
+
+//            GameServerConfigService gameServerConfigService = LocalMananger.getInstance().getGameServerConfigService();
+//            if(gameServerConfigService.getGameServerConfig().getServerType() == BOConst.BO_WORLD){
+//                logger.info("World Server started");
+//            }else {
+//                logger.info("Game Server started");
+//            }
+        } else {
+//            // Xiong 管理功能
+//            Admin adm = Admin.make(configFile, admCmd);
+//            adm.run(admArgs);
+        }
+    }
+}
---
+++ src/main/java/com/wolf/shoot/server/ServerStatusLog.java
@@ -0,0 +1,131 @@
+package com.wolf.shoot.server;
+
+import com.wolf.shoot.util.ServerVersion;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 服务器状态日志
+ *
+ *
+ *
+ */
+public class ServerStatusLog {
+    /** 服务器正在启动 */
+    private static final int STARTING = 0;
+    /** 服务器启动失败,已经终止 */
+    private static final int START_FAIL = 1;
+    /** 服务器启动成功,正在运行 */
+    private static final int RUNNING = 2;
+    /** 服务器正在停止 */
+    private static final int STOPPING = 3;
+    /** 服务器已经停止 */
+    private static final int STOPPED = 4;
+    /** 服务器状态日志保存地路径 */
+    private final String serverStatusLog;
+    /** 默认的系统状态保存地址,保存地址为:玩家当前所在目录/server_status */
+    private static final ServerStatusLog instance = new ServerStatusLog(System
+            .getProperty("user.dir")
+            + File.separator + "logs" + File.separator + "server_status");
+
+    /**
+     *
+     * @param serverStatusLog
+     */
+    public ServerStatusLog(String serverStatusLog) {
+        if (serverStatusLog == null
+                || (serverStatusLog = serverStatusLog.trim()).length() == 0) {
+            throw new IllegalArgumentException(
+                    "The threadNumberPerPort must not be null ");
+        }
+        this.serverStatusLog = serverStatusLog;
+        this.checkLogDir();
+    }
+
+    /**
+     * 服务器正在启动
+     */
+    public void logStarting() {
+        this.writeStatusLog(STARTING);
+    }
+
+    /**
+     * 服务器启动失败
+     */
+    public void logStartFail() {
+        this.writeStatusLog(START_FAIL);
+    }
+
+    /**
+     * 服务器正在运行
+     */
+    public void logRunning() {
+        this.writeStatusLog(RUNNING);
+    }
+
+    /**
+     * 服务器正在停止
+     */
+    public void logStoppping() {
+        this.writeStatusLog(STOPPING);
+    }
+
+    /**
+     * 服务器已经停止
+     */
+    public void logStopped() {
+        this.writeStatusLog(STOPPED);
+    }
+
+    /**
+     * 取得默认的服务器状态日志
+     *
+     * @return
+     */
+    public static ServerStatusLog getDefaultLog() {
+        return instance;
+    }
+
+    private void writeStatusLog(int status) {
+        File _logFile = new File(this.serverStatusLog);
+        FileOutputStream _out = null;
+        try {
+            _out = new FileOutputStream(_logFile);
+            String _time = new SimpleDateFormat("yyyy-MM-dd HH:mm")
+                    .format(new Date());
+            String _status = status + "\t" + _time + "\t" + ServerVersion.getServerVersion();
+            _out.write(_status.getBytes(Charset.forName("ISO8859-1")));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (_out != null) {
+                try {
+                    _out.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+    }
+
+    private void checkLogDir() {
+        File _logFile = new File(this.serverStatusLog).getAbsoluteFile();
+        if (_logFile.exists()) {
+            return;
+        }
+        File _parentFile = _logFile.getParentFile();
+        if (_parentFile.exists()) {
+            return;
+        }
+        if (_parentFile.mkdirs()) {
+            return;
+        } else {
+            System.err.println("Can't create the server status log dir["
+                    + _parentFile + "]");
+        }
+    }
+}
