6498dfbc5ecef762c58c31266c7c4b77fc5a2556
jiangwenping <jwpttcg66>
2017-02-07 15:19:45 +0800

    优化服务器

--- src/main/config/game_server_diff.cfg.js
+++ src/main/config/game_server_diff.cfg.js
@@ -7,42 +7,10 @@ config.roomLifcCycleTime=60 * 60 * 2;
 config.txStressTest=0;
 //每秒钟处理消息数量
 config.session_prcoss_message_max_size=10;
-
-/** 房间更新最小线程数量*/
-config.roomUpdateCoreThreads = 500;
-/**房间更新最大线程数量*/
-config.roomUpdateMaxThreads = 6000;
-/**房间更新最大信号量*/
-config.roomSemaphoreMaxSize = 500;
-config.roomUpdateRefreshDistanceTime=1000;
-
-config.sessionUpdateProduceThreadsSize=2;
-config.sessionUpdateConsumeThreadsSize=10;
-
-config.roomUpdateProduceThreadsSize=2;
-config.roomUpdateConsumeThreadsSize=20;
-
-config.sessionUpdateSingleUseTimeMaxSize=200;
-/**房间更新单次最多占用时间(ms)*/
-config.roomUpdateSingleUseTimeMaxSize = 200;
-
-config.queueDispatchFlag=false;
-config.commonResultCacheFlag=true;
-config.sessionAddProductNullSleepTime = 1;
-config.roomAddProductNullSleepTime = 1;
-config.tcpHeartKeepAliveTime=60;
-config.sessionUpdateAllUseTimeMinSize=600;
-config.roomUpdateAllUseTimeMinSize=600;
-config.sessionUpdateAllUseSleepTime=1;
-config.roomUpdateAllUseSleepTime=1;
-
 config.globalUpdateFlag=true;
 config.globalUpdateProduceThreadsSize=1;
 config.globalUpdateConsumeThreadsSize=10;
 config.globalUpdateAllUseTimeMinSize=600;
 config.globalUpdateAllUseSleepTime=1;
-
 config.prePutWorldMatchFlag=false;
 config.communicationTcpHeartKeepAliveTime=60*60*24;
-
-
