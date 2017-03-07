54d72a224b286b45937c68fc214fdda704403e87
jwp <jwpttcg66@126.com>
2017-03-07 16:24:34 +0800

    增加rpc服务

---
+++ src/main/java/com/wolf/shoot/service/net/message/encoder/RpcEncoder.java
@@ -0,0 +1,28 @@
+package com.wolf.shoot.service.net.message.encoder;
+
+import com.wolf.shoot.common.util.SerializationUtil;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+
+/**
+ * Created by jwp on 2017/3/7.
+ */
+public class RpcEncoder extends MessageToByteEncoder {
+
+    private Class<?> genericClass;
+
+    public RpcEncoder(Class<?> genericClass) {
+        this.genericClass = genericClass;
+    }
+
+    @Override
+    public void encode(ChannelHandlerContext ctx, Object in, ByteBuf out) throws Exception {
+        if (genericClass.isInstance(in)) {
+            byte[] data = SerializationUtil.serialize(in);
+            //byte[] data = JsonUtil.serialize(in); // Not use this, have some bugs
+            out.writeInt(data.length);
+            out.writeBytes(data);
+        }
+    }
+}
