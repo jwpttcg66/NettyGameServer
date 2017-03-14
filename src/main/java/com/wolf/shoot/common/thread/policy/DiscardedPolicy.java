a2681416db42824f46530dc4ef9559d70998771e
jiangwenping <jwpttcg66>
2017-03-14 10:38:38 +0800

    修正线程策略

--- src/main/java/com/wolf/shoot/common/thread/policy/DiscardedPolicy.java
+++ src/main/java/com/wolf/shoot/common/thread/policy/DiscardedPolicy.java
@@ -3,14 +3,12 @@ package com.wolf.shoot.common.thread.policy;
 import com.wolf.shoot.common.constant.Loggers;
 import org.slf4j.Logger;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * Created by jwp on 2017/3/8.
  */
-public class DiscardedPolicy implements RejectedExecutionHandler {
+public class DiscardedPolicy extends ThreadPoolExecutor.DiscardOldestPolicy {
     private static final Logger logger = Loggers.threadLogger;
 
     private String threadName;
@@ -28,15 +26,7 @@ public class DiscardedPolicy implements RejectedExecutionHandler {
             logger.error("RPC Thread pool [{}] is exhausted, executor={}", threadName, executor.toString());
         }
 
-        if (!executor.isShutdown()) {
-            BlockingQueue<Runnable> queue = executor.getQueue();
-            int discardSize = queue.size() >> 1;
-            for (int i = 0; i < discardSize; i++) {
-                queue.poll();
-            }
-
-            queue.offer(runnable);
-        }
+        super.rejectedExecution(runnable, executor);
     }
 }
 
--- src/main/java/com/wolf/shoot/common/thread/policy/RejectedRunnable.java
+++
@@ -1,8 +0,0 @@
-package com.wolf.shoot.common.thread.policy;
-
-/**
- * Created by jwp on 2017/3/8.
- */
-public interface RejectedRunnable extends Runnable {
-    void rejected();
-}
