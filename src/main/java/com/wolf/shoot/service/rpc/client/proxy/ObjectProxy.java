5280b5f6bcd36682f4129a1bd68599abc1742993
jiangwenping <jwpttcg66>
2017-03-16 11:10:17 +0800

    rpc配置

--- src/main/java/com/wolf/shoot/service/rpc/client/proxy/ObjectProxy.java
+++ src/main/java/com/wolf/shoot/service/rpc/client/proxy/ObjectProxy.java
@@ -25,20 +25,21 @@ public class ObjectProxy<T> implements InvocationHandler{
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        if (Object.class == method.getDeclaringClass()) {
-            String name = method.getName();
-            if ("equals".equals(name)) {
-                return proxy == args[0];
-            } else if ("hashCode".equals(name)) {
-                return System.identityHashCode(proxy);
-            } else if ("toString".equals(name)) {
-                return proxy.getClass().getName() + "@" +
-                        Integer.toHexString(System.identityHashCode(proxy)) +
-                        ", with InvocationHandler " + this;
-            } else {
-                throw new IllegalStateException(String.valueOf(method));
-            }
-        }
+        //基本上用不到equql, hashcode, tostring等底层函数
+//        if (Object.class == method.getDeclaringClass()) {
+//            String name = method.getName();
+//            if ("equals".equals(name)) {
+//                return proxy == args[0];
+//            } else if ("hashCode".equals(name)) {
+//                return System.identityHashCode(proxy);
+//            } else if ("toString".equals(name)) {
+//                return proxy.getClass().getName() + "@" +
+//                        Integer.toHexString(System.identityHashCode(proxy)) +
+//                        ", with InvocationHandler " + this;
+//            } else {
+//                throw new IllegalStateException(String.valueOf(method));
+//            }
+//        }
 
         RpcRequest request = new RpcRequest();
         request.setRequestId(UUID.randomUUID().toString());
