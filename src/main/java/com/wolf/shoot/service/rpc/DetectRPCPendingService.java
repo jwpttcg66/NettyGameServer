d482c837a44913c43f31b38ee88ab7a12c7fb1de
gouboting <gouboting@5agame.cn>
2017-03-28 18:05:13 +0800

    删掉没用的

--- src/main/java/com/wolf/shoot/service/rpc/DetectRPCPendingService.java
+++ src/main/java/com/wolf/shoot/service/rpc/DetectRPCPendingService.java
@@ -3,7 +3,6 @@ package com.wolf.shoot.service.rpc;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
