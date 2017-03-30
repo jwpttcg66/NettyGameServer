ab885ec76bb47f35bc11fbd41d50cbb2e9743dfd
jiangwenping <jwpttcg66>
2017-03-30 12:27:43 +0800

    增加zookeeper

---
+++ src/main/java/com/wolf/shoot/service/rpc/zookeeper/ZooKeepeerConfig.java
@@ -0,0 +1,10 @@
+package com.wolf.shoot.service.rpc.zookeeper;
+
+import com.wolf.shoot.common.config.AbstractGameConfigure;
+
+/**
+ * Created by jiangwenping on 17/3/30.
+ */
+public class ZooKeepeerConfig extends AbstractGameConfigure {
+
+}
