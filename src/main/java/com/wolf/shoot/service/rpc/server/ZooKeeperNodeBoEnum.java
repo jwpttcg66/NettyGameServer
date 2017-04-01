c5e9bd5c4c626d6993bdaf0dbcb47b3feda5e3a5
jiangwenping <jwpttcg66@126.com>
2017-04-01 15:36:27 +0800

    优化rpc

--- src/main/java/com/wolf/shoot/service/rpc/server/ZooKeeperNodeBoEnum.java
+++ src/main/java/com/wolf/shoot/service/rpc/server/ZooKeeperNodeBoEnum.java
@@ -5,7 +5,7 @@ import com.wolf.shoot.common.constant.BOEnum;
 /**
  * Created by jiangwenping on 17/3/31.
  */
-public enum  ZooKeeperNodeInfo {
+public enum ZooKeeperNodeBoEnum {
     world(BOEnum.WORLD, "world_registry_adress"),
     game(BOEnum.GAME, "game_registry_adress"),
     db(BOEnum.DB, "db_registry_adress"),
@@ -13,7 +13,7 @@ public enum  ZooKeeperNodeInfo {
     private BOEnum boEnum;
     private String registryAdress;
 
-    ZooKeeperNodeInfo(BOEnum boEnum, String registryAdress) {
+    ZooKeeperNodeBoEnum(BOEnum boEnum, String registryAdress) {
         this.boEnum = boEnum;
         this.registryAdress = registryAdress;
     }
