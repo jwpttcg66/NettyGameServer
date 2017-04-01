716dcbc199fe2bba624180d7cbcbf1ac1add9e59
jiangwenping <jwpttcg66>
2017-04-01 19:05:43 +0800

    优化rpc

--- src/test/java/com/wolf/shoot/rpc/client/zookeeper/ZookeeperTest.java
+++ src/test/java/com/wolf/shoot/rpc/client/zookeeper/ZookeeperTest.java
@@ -6,6 +6,7 @@ import com.wolf.shoot.common.util.BeanUtil;
 import com.wolf.shoot.manager.LocalMananger;
 import com.wolf.shoot.manager.spring.LocalSpringBeanManager;
 import com.wolf.shoot.manager.spring.LocalSpringServiceManager;
+import com.wolf.shoot.manager.spring.LocalSpringServicerAfterManager;
 import com.wolf.shoot.service.rpc.client.ZookeeperRpcServiceDiscovery;
 import com.wolf.shoot.service.rpc.server.RpcConfig;
 import com.wolf.shoot.service.rpc.server.SdRpcServiceProvider;
@@ -39,10 +40,13 @@ public class ZookeeperTest {
     public void init() {
         LocalSpringServiceManager localSpringServiceManager = (LocalSpringServiceManager) BeanUtil.getBean("localSpringServiceManager");
         LocalSpringBeanManager localSpringBeanManager = (LocalSpringBeanManager) BeanUtil.getBean("localSpringBeanManager");
+        LocalSpringServicerAfterManager localSpringServicerAfterManager  = (LocalSpringServicerAfterManager) BeanUtil.getBean("localSpringServicerAfterManager");
         LocalMananger.getInstance().setLocalSpringBeanManager(localSpringBeanManager);
         LocalMananger.getInstance().setLocalSpringServiceManager(localSpringServiceManager);
+        LocalMananger.getInstance().setLocalSpringServicerAfterManager(localSpringServicerAfterManager);
         try {
             localSpringServiceManager.start();
+            localSpringServicerAfterManager.start();
         } catch (Exception e) {
             e.printStackTrace();
         }
