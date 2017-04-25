778889e720fb1506d9ebfb2e3f3bf107b7d0c9dc
gouboting <gouboting@5agame.cn>
2017-04-25 11:48:32 +0800

    ZookeeperRpcServiceDiscovery中加入CuratorFrame进行节点状态监控

--- src/main/java/com/wolf/shoot/service/rpc/client/ZookeeperRpcServiceDiscovery.java
+++ src/main/java/com/wolf/shoot/service/rpc/client/ZookeeperRpcServiceDiscovery.java
@@ -10,9 +10,22 @@ import com.wolf.shoot.manager.LocalMananger;
 import com.wolf.shoot.service.IService;
 import com.wolf.shoot.service.rpc.server.zookeeper.ZooKeeperNodeBoEnum;
 import com.wolf.shoot.service.rpc.server.zookeeper.ZooKeeperNodeInfo;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.ACLProvider;
+import org.apache.curator.framework.recipes.cache.ChildData;
+import org.apache.curator.framework.recipes.cache.TreeCache;
+import org.apache.curator.framework.recipes.cache.TreeCacheEvent;
+import org.apache.curator.framework.recipes.cache.TreeCacheListener;
+import org.apache.curator.retry.RetryNTimes;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.ZooDefs.Perms;
+import org.apache.zookeeper.data.ACL;
+import org.apache.zookeeper.data.Id;
 import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +35,8 @@ import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by jiangwenping on 17/3/30.
@@ -40,12 +55,24 @@ public class ZookeeperRpcServiceDiscovery implements IService{
 
     private ZooKeeper zk;
 
+    private CuratorFramework client;
+    
     public void discovery(ZooKeeperNodeBoEnum zooKeeperNodeBoEnum){
         if(zk == null){
             zk = connectServer();
             if (zk != null) {
                 watchNode(zooKeeperNodeBoEnum);
             }
+        }
+        if(client == null){
+        	client = creatClient();
+        	if(client != null){
+        		try {
+					setListenter(client,zooKeeperNodeBoEnum);
+				} catch (Exception e) {
+					logger.debug("Create CuratorFramework Exception:"+e.getMessage());
+				}
+        	}
         }
     }
 
@@ -164,4 +191,82 @@ public class ZookeeperRpcServiceDiscovery implements IService{
             }
         }
     }
+    private static CuratorFramework creatClient() {
+		ACLProvider aclProvider = new ACLProvider() {
+			private List<ACL> acl;
+
+			@Override
+			public List<ACL> getDefaultAcl() {
+				if (acl == null) {
+					ArrayList<ACL> acl = ZooDefs.Ids.CREATOR_ALL_ACL;
+					acl.clear();
+					acl.add(new ACL(Perms.ALL, new Id("auth", "admin:admin")));
+					this.acl = acl;
+				}
+				return acl;
+			}
+
+			@Override
+			public List<ACL> getAclForPath(String path) {
+				return acl;
+			}
+		};
+		//String scheme = "digest";
+		//byte[] auth = "admin:admin".getBytes();
+		int connectionTimeoutMs = 5000;
+		//String connectString = "192.168.0.158:2181";
+		GameServerConfigService gameServerConfigService = LocalMananger.getInstance().getLocalSpringServiceManager().getGameServerConfigService();
+        ZooKeeperConfig zooKeeperConfig = gameServerConfigService.getZooKeeperConfig();
+        String registryAdress = zooKeeperConfig.getProperty(GlobalConstants.ZooKeeperConstants.registryAdress);
+		String namespace = "";
+		CuratorFramework client = CuratorFrameworkFactory.builder()
+				.aclProvider(aclProvider)//.authorization(scheme, auth)
+				.connectionTimeoutMs(connectionTimeoutMs)
+				.connectString(registryAdress)//.namespace(namespace)
+				.retryPolicy(new RetryNTimes(Integer.MAX_VALUE, 1000)).build();
+		client.start();
+		return client;
+	}
+    private static void setListenter(CuratorFramework client,ZooKeeperNodeBoEnum zooKeeperNodeBoEnum)throws Exception {
+		ExecutorService pool = Executors.newCachedThreadPool();
+		TreeCache cache = new TreeCache(client, zooKeeperNodeBoEnum.getRootPath());
+		cache.getListenable().addListener(new TreeCacheListener() {
+			@Override
+			public void childEvent(CuratorFramework client, TreeCacheEvent event)
+					throws Exception {
+				ChildData data = event.getData();
+				if (data != null) {
+					switch (event.getType()) {
+					case NODE_ADDED:
+						logger.debug("NODE_ADDED : " + data.getPath()+ "  数据:" + new String(data.getData()));
+						break;
+					case NODE_REMOVED:
+						logger.debug("NODE_REMOVED : " + data.getPath()+ "  数据:" + new String(data.getData()));
+						break;
+					case NODE_UPDATED:
+						logger.debug("NODE_UPDATED : " + data.getPath()+ "  数据:" + new String(data.getData()));
+						break;
+					default:
+						break;
+					}
+				} else {
+					switch (event.getType()) {
+					case CONNECTION_SUSPENDED:
+						logger.debug("data is null : " + "CONNECTION_SUSPENDED");
+						break;
+					case CONNECTION_RECONNECTED:
+						logger.debug("data is null : " + "CONNECTION_RECONNECTED");
+						break;
+					case CONNECTION_LOST:
+						logger.debug("data is null : " + "CONNECTION_LOST");
+						break;
+					default:
+						break;
+					}
+				}
+			}
+		});
+		// 开始监听
+		cache.start();
+	}
 }
