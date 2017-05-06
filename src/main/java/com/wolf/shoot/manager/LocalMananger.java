8cea65b699589cb97c5681f0a2a2fa13030c2c7a
jwp <jwpttcg66@126.com>
2017-05-06 15:14:41 +0800

    增加扩展

--- src/main/java/com/wolf/shoot/manager/LocalMananger.java
+++ src/main/java/com/wolf/shoot/manager/LocalMananger.java
@@ -24,13 +24,9 @@ import java.util.Map;
  * @author b053-mac
  *	本地服务管理
  */
-public class LocalMananger implements ILocalManager{
-
-    protected static final Logger log = Loggers.serverLogger;
+public class LocalMananger extends AbstractLocalManager{
 
     public static LocalMananger instance = new LocalMananger();
-
-    protected static Map<Class,Object> services;
 
     public LocalMananger() {
         services = new LinkedHashMap<Class,Object>(40,0.5f);
@@ -71,20 +67,8 @@ public class LocalMananger implements ILocalManager{
     }
 
     @Override
-    public <X,Y extends X> void create(Class<Y> clazz,Class<X> inter) throws Exception{
-        log.info(clazz.getSimpleName() + " is create");
-        Object newObject = clazz.newInstance();
-        if(newObject instanceof IService){
-            ((IService)newObject).startup();
-        }
-        add(newObject,inter);
-    }
-
-    @Override
     public <T> void add(Object service, Class<T> inter) {
-        log.info(service.getClass().getSimpleName() + " is add");
-        if (service.getClass() != inter && !inter.isAssignableFrom(service.getClass())) //接口和实现类必须相等或者继承关系
-            throw new IllegalArgumentException();
+        super.add(service, inter);
         if (service instanceof GameTcpMessageProcessor) {
             this.gameTcpMessageProcessor = (GameTcpMessageProcessor) service;
         } else if (service instanceof GameUdpMessageOrderProcessor) {
@@ -97,30 +81,6 @@ public class LocalMananger implements ILocalManager{
             this.asyncEventService = (AsyncEventService) service;
         }
 
-        services.put(inter, service);
-
-    }
-
-
-    @Override
-    public <T> T get(Class<T> clazz) {
-        return (T)services.get(clazz);
-    }
-
-    @Override
-    public void shutdown() {
-//        Object[] ss = new Object[services.size()];
-//        services.values().toArray(ss);
-        Object[] ss = services.values().toArray(new Object[0]);
-        for(int i=ss.length-1;i>0;i--){
-            if(ss[i] instanceof IService) {
-                try {
-                    ((IService)ss[i]).shutdown();
-                } catch (Exception e) {
-                    Loggers.errorLogger.error(e.toString(),e);
-                }
-            }
-        }
     }
 
     public GameTcpMessageProcessor getGameTcpMessageProcessor() {
