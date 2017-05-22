99db9d3fdaabe1d10623816284c9e08c98f5d434
jiangwenping <jwpttcg66>
2017-05-22 14:34:44 +0800

    增加游戏内的事件全局服务

---
+++ src/main/java/com/snowcattle/game/service/event/SingleEventConstants.java
@@ -0,0 +1,10 @@
+package com.snowcattle.game.service.event;
+
+import com.snowcattle.game.executor.event.EventType;
+
+/**
+ * Created by jiangwenping on 2017/5/22.
+ */
+public class SingleEventConstants {
+    public static EventType singleRunEventType = new EventType(1001);
+}
