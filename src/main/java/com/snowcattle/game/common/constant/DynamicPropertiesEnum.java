8e040ee531e0e05f4048214a55ca43604b1f341b
jiangwenping <jwpttcg66>
2017-05-31 15:55:04 +0800

    拓展config

--- src/main/java/com/snowcattle/game/common/constant/DynamicPropertiesEnum.java
+++ src/main/java/com/snowcattle/game/common/constant/DynamicPropertiesEnum.java
@@ -2,7 +2,7 @@ package com.snowcattle.game.common.constant;
 
 /**
  * Created by jiangwenping on 17/2/22.
- * 默认的检查标志
+ * 动态属性
  */
 public enum  DynamicPropertiesEnum {
     udp_message_tocken_check_flag,
