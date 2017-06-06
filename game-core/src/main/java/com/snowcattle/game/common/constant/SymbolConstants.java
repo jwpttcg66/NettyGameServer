4385106c9b8fd37a3da827c76816e79378dcf4fb
jiangwenping <jwpttcg66>
2017-06-06 17:53:16 +0800

    加入常用的特殊字符

---
+++ game-core/src/main/java/com/snowcattle/game/common/constant/SymbolConstants.java
@@ -0,0 +1,19 @@
+package com.snowcattle.game.common.constant;
+
+/**
+ * Created by jiangwenping on 2017/6/6.
+ * 常用符号
+ */
+public class SymbolConstants {
+    public static final String SEMICOLON=";";//分号
+    public static final String SEMICOLON_REG=";|;";//分号
+    public static final String COLON=":";//冒号
+    public static final String COLON_REG=":|:";//冒号
+    public static final String COMMA=",";
+    public static final String COMMA_REG=",|,";
+    public static final String XIEGANG_REG="/";
+    public static final String SHUXIAN_REG="|";
+    public static final String UNDERLINE_REG="_";
+    public static final String JINGHAO_REG="#";
+    public static final String AT_REG="@";
+}
