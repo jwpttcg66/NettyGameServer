17fc2c23ec8d3b41542d28ba966887942f091fb2
jiangwenping <jwpttcg66>
2017-06-28 17:28:59 +0800

    网络代理

---
+++ game-core/src/test/java/com/snowcattle/game/net/client/proxy/ProxyClient.java
@@ -0,0 +1,20 @@
+package com.snowcattle.game.net.client.proxy;
+
+import com.snowcattle.game.TestStartUp;
+import com.snowcattle.game.manager.LocalMananger;
+import com.snowcattle.game.manager.spring.LocalSpringServiceManager;
+import com.snowcattle.game.net.client.tcp.GameClient;
+import com.snowcattle.game.service.net.message.registry.MessageRegistry;
+
+/**
+ * Created by jiangwenping on 2017/6/28.
+ */
+public class ProxyClient extends GameClient {
+    public static void main(String[] args) throws Exception {
+        TestStartUp.startUp();
+        LocalSpringServiceManager localSpringServiceManager = LocalMananger.getInstance().getLocalSpringServiceManager();
+        localSpringServiceManager.setMessageRegistry(LocalMananger.getInstance().get(MessageRegistry.class));
+
+        new GameClient().connect("127.0.0.1", 9090);
+    }
+}
