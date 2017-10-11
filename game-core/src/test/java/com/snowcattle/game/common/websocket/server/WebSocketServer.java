32bfa717a0f9ec6bb8c42bb7aa2ba23ad249460a
jiangwenping <jwpttcg66>
2017-10-11 15:59:09 +0800

    增加http解析

--- game-core/src/test/java/com/snowcattle/game/common/websocket/server/WebSocketServer.java
+++ game-core/src/test/java/com/snowcattle/game/common/websocket/server/WebSocketServer.java
@@ -47,8 +47,8 @@ import io.netty.handler.ssl.util.SelfSignedCertificate;
  */
 public final class WebSocketServer {
 
-//    static final boolean SSL = System.getProperty("ssl") != null;
-    static final boolean SSL = true;
+    static final boolean SSL = System.getProperty("ssl") != null;
+//    static final boolean SSL = true;
     static final int PORT = Integer.parseInt(System.getProperty("port", SSL? "8443" : "8080"));
 
     public static void main(String[] args) throws Exception {
